13a0eaef3424ddfb915893b2a2aa9087efa683d8
Lee.Ryan.Campbell <Lee.Ryan.Campbell@be1c8d10-64b7-54c5-464f-300c71bfaafb>
2010-10-03 22:08:45 +0000

    Adding the SchedulingExamples

---
+++ src/RxSamples.ConsoleApp/EnumerableExamples.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RxSamples.ConsoleApp
+{
+  class EnumerableExamples
+  {
+    public void IEnumerableExample()
+    {
+      Console.WriteLine("--Starting Lazy Evaluation--");
+      Do(LazyEvaluation());
+      Console.WriteLine("--Starting Eager Evaluation--");
+      Do(EagerEvaluation());
+
+      Console.WriteLine("--Starting Lazy Evaluation with Take(1)--");
+      LazyEvaluation().Take(1).Run(Console.WriteLine);
+
+      Console.ReadKey();
+    }
+
+    private void Do(IEnumerable<int> list)
+    {
+      foreach (var i in list)
+      {
+        Console.WriteLine("Read out first value of {0}", i);
+        break;
+      }
+    }
+
+    public IEnumerable<int> LazyEvaluation()
+    {
+      Console.WriteLine("About to return 1");
+      yield return 1;
+      Console.WriteLine("About to return 2");
+      yield return 2;
+    }
+
+    public IEnumerable<int> EagerEvaluation()
+    {
+      var result = new List<int>();
+      Console.WriteLine("About to return 1");
+      result.Add(1);
+      Console.WriteLine("About to return 2");
+      result.Add(2);
+      return result;
+    }
+  }
+}
---
+++ src/RxSamples.ConsoleApp/SchedulingExamples.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Concurrency;
+using System.Linq;
+using System.Threading;
+
+namespace RxSamples.ConsoleApp
+{
+  class SchedulingExamples
+  {
+    //public void IntroToScheduling()
+    //{
+    //  Scheduler.CurrentThread;
+    //  Scheduler.Dispatcher;
+    //  Scheduler.Immediate;
+    //  Scheduler.NewThread;
+    //  Scheduler.TaskPool;
+    //  Scheduler.ThreadPool;
+    //}
+
+    public void Rx_can_still_Deadlock()
+    {
+      var stream = new Subject<int>();
+      Console.WriteLine("Next line should dead lock the system.");
+      var value = stream.First();
+      stream.OnNext(1);
+      Console.WriteLine("I can never execute....");
+    }
+
+    private static void ScheduleTasks(IScheduler scheduler)
+    {
+      Action leafAction = () => Console.WriteLine("leafAction.");
+      Action innerAction = () =>
+                             {
+                               Console.WriteLine("innerAction start.");
+                               scheduler.Schedule(leafAction);
+                               Console.WriteLine("innerAction end.");
+                             };
+      Action outerAction = () =>
+                             {
+                               Console.WriteLine("outer start.");
+                               scheduler.Schedule(innerAction);
+                               Console.WriteLine("outer end.");
+                             };
+      scheduler.Schedule(outerAction);
+    }
+
+    public void Scheduling_on_Immediate_is_sequential()
+    {
+      ScheduleTasks(Scheduler.Immediate);
+      Console.ReadLine();
+      /*Output:
+       * outer start.
+       * outer end.
+       * innerAction start.
+       * innerAction end.
+       * leafAction.
+       */
+    }
+
+    public void Scheduling_on_the_CurrentThread_is_dispatched_to_a_Trampoline()
+    {
+      ScheduleTasks(Scheduler.CurrentThread);
+      Console.ReadLine();
+      /*Output:
+       * outer start.
+       * outer end.
+       * innerAction start.
+       * innerAction end.
+       * leafAction.
+       */
+    }
+
+    public void DefaultSchedulingOnObservableCreate()
+    {
+      var stream = Observable.Create<int>(
+        o =>
+          {
+            Console.WriteLine("Subscription on thread{0}", Thread.CurrentThread.ManagedThreadId);
+            o.OnNext(1);
+            o.OnNext(2);
+            o.OnNext(3);
+            return () => { };
+          });
+
+      Console.WriteLine("Main thread {0}", Thread.CurrentThread.ManagedThreadId);
+      stream
+        .SubscribeOn(Scheduler.NewThread)
+        .ObserveOn(Scheduler.NewThread)
+        .Subscribe(
+          i => Console.WriteLine("Value={0} on thread:{1}", i, Thread.CurrentThread.ManagedThreadId),
+          () => Console.WriteLine("Oncomplete on thread:{0}", Thread.CurrentThread.ManagedThreadId));
+    }
+
+    /// <summary>
+    /// Use this method to see how Scheduling is performed. The sheduler and thread will written to the console on subscription and publication to the Observer.
+    /// </summary>
+    /// <param name="subscribeOnScheduler"></param>
+    /// <param name="observeOnScheduler"></param>
+    public void SchedulingOnObservableCreate(IScheduler subscribeOnScheduler, IScheduler observeOnScheduler)
+    {
+      Console.WriteLine("Subscribing on {0} and Observing on {1}", subscribeOnScheduler.GetType().Name, observeOnScheduler.GetType().Name);
+      var stream = Observable.Create<int>(
+        o =>
+          {
+            Console.WriteLine("Subscription on thread{0}", Thread.CurrentThread.ManagedThreadId);
+            o.OnNext(1);
+            o.OnNext(2);
+            o.OnNext(3);
+            return () => { };
+          });
+
+      Console.WriteLine("Main thread {0}", Thread.CurrentThread.ManagedThreadId);
+      stream
+        .SubscribeOn(subscribeOnScheduler)
+        .ObserveOn(observeOnScheduler)
+        .Subscribe(
+          i => Console.WriteLine("Value={0} on thread:{1}", i, Thread.CurrentThread.ManagedThreadId),
+          () => Console.WriteLine("Oncomplete on thread:{0}", Thread.CurrentThread.ManagedThreadId));
+    }
+  }
+}
