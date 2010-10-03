738f4460cc9789fd738fdf9d5b1df4380c89e7e2
Lee.Ryan.Campbell <Lee.Ryan.Campbell@be1c8d10-64b7-54c5-464f-300c71bfaafb>
2010-10-03 21:52:32 +0000

    Initial check in of RxSamples used to create the blog series

---
+++ trunk/src/RxSamples.ConsoleApp/01_SubjectExamples.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RxSamples.ConsoleApp
+{
+  class SubjectExamples : ExamplesBase
+  {
+    public void SubjectExample()
+    {
+      var subject = new Subject<string>();
+
+      WriteStreamToConsole(subject);
+
+      subject.OnNext("a");
+      subject.OnNext("b");
+      subject.OnNext("c");
+    }
+
+    public void ReplaySubjectExample()
+    {
+      var subject = new ReplaySubject<string>();
+
+      subject.OnNext("a");
+      WriteStreamToConsole(subject);
+
+      subject.OnNext("b");
+      subject.OnNext("c");
+    }
+
+    public void AsyncSubjectExample()
+    {
+      var subject = new AsyncSubject<string>();
+
+      subject.OnNext("a");
+      WriteStreamToConsole(subject);
+      subject.OnNext("b");
+      subject.OnNext("c");
+      subject.OnCompleted();
+    }
+
+    public void BehaviorSubjectExample()
+    {
+      //Need to provide a default value.
+      var subject = new BehaviorSubject<string>("a");
+
+      subject.OnNext("b");
+      subject.OnNext("c");
+      subject.OnNext("d");
+      subject.OnCompleted();
+      WriteStreamToConsole(subject);
+    }
+  }
+}
---
+++ trunk/src/RxSamples.ConsoleApp/ConsoleColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RxSamples.ConsoleApp
+{
+  /// <summary>
+  /// Creates a scope for a console foreground color. When disposed, will return to the previous <see cref="Console.ForegroundColor"/>.
+  /// </summary>
+  public class ConsoleColor :IDisposable
+  {
+    private readonly System.ConsoleColor _previousColor;
+
+    public ConsoleColor(System.ConsoleColor color)
+    {
+      _previousColor = Console.ForegroundColor;
+      Console.ForegroundColor = color;
+    }
+
+    public void Dispose()
+    {
+      Console.ForegroundColor = _previousColor;
+    }
+  }
+}
