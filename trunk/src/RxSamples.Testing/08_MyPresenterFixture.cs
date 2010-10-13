1124a84070c4df94ccb3879aa77c83d0e1320f30
Lee.Ryan.Campbell <Lee.Ryan.Campbell@be1c8d10-64b7-54c5-464f-300c71bfaafb>
2010-10-13 21:50:46 +0000

    Adding the TestScheduler and the .NET 4 code

---
+++ trunk/src/RxSamples.Testing/08_MyPresenterFixture.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Concurrency;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RxSamples.ConsoleApp;
+
+namespace RxSamples.Testing
+{
+    [TestClass]
+    public class MyPresenterFixture
+    {
+        private Mock<IMyService> _myServiceMock;
+        private Mock<IViewModel> _viewModelMock;
+        private Mock<ISchedulerService> _schedulerServiceMock;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _myServiceMock = new Mock<IMyService>();
+            _viewModelMock = new Mock<IViewModel>();
+            _schedulerServiceMock = new Mock<ISchedulerService>();
+
+            _schedulerServiceMock.SetupGet(ss => ss.Dispatcher).Returns(Scheduler.Immediate);
+            _schedulerServiceMock.SetupGet(ss => ss.ThreadPool).Returns(Scheduler.Immediate);
+            var prices = new ObservableCollection<decimal>();
+            _viewModelMock.SetupGet(vm => vm.Prices).Returns(prices);
+        }
+
+        [TestMethod]
+        public void Should_pass_symbol_to_MyService_PriceStream()
+        {
+            var expected = "SomeSymbol";
+            var priceStream = new Subject<decimal>();
+            _myServiceMock.Setup(svc => svc.PriceStream(It.Is<string>(symbol=>symbol==expected))).Returns(priceStream);
+            var prices = new ObservableCollection<decimal>();
+            _viewModelMock.SetupGet(vm => vm.Prices).Returns(prices);
+
+            var sut = new MyPresenter(_myServiceMock.Object, _viewModelMock.Object, _schedulerServiceMock.Object);
+            sut.Show(expected);
+
+            _myServiceMock.Verify();
+        }
+
+
+        [TestMethod]
+        public void Should_add_to_VM_Prices_when_MyService_publishes_price()
+        {
+            var priceStream = new Subject<decimal>();
+            _myServiceMock.Setup(svc => svc.PriceStream(It.IsAny<string>())).Returns(priceStream);
+            var prices = new ObservableCollection<decimal>();
+            _viewModelMock.SetupGet(vm => vm.Prices).Returns(prices);
+
+            var sut = new MyPresenter(_myServiceMock.Object, _viewModelMock.Object, _schedulerServiceMock.Object);
+            sut.Show("SomeSymbol");
+            decimal expected = 1.23m;
+            priceStream.OnNext(expected);
+
+            Assert.AreEqual(1, prices.Count);
+            Assert.AreEqual(expected, prices.First());
+        }
+
+        [TestMethod]
+        public void Should_subscribe_to_prices_on_threadPool()
+        {
+            decimal expected = 1.23m;
+            var threadPoolScheduler = new TestScheduler();
+            _schedulerServiceMock.SetupGet(ss => ss.ThreadPool).Returns(threadPoolScheduler);
+            var priceStream = new Subject<decimal>();
+            _myServiceMock.Setup(svc => svc.PriceStream(It.IsAny<string>())).Returns(priceStream);
+            var sut = new MyPresenter(_myServiceMock.Object, _viewModelMock.Object, _schedulerServiceMock.Object);
+            sut.Show("SomeSymbol");
+
+
+            priceStream.OnNext(expected);
+            //this should yeild 0 rows. If Done correctly as per below then it would be count==1...
+            //threadPoolScheduler.Schedule(() => priceStream.OnNext(expected));
+            //threadPoolScheduler.Run();
+
+            Assert.AreEqual(0, _viewModelMock.Object.Prices.Count);
+        }
+
+        [TestMethod]
+        public void Should_observe_on_dispatcher()
+        {
+            decimal expected = 1.23m;
+            var threadPoolScheduler = new TestScheduler();
+            _schedulerServiceMock.SetupGet(ss => ss.ThreadPool).Returns(threadPoolScheduler);
+            var dispatcherScheduler = new TestScheduler();
+            _schedulerServiceMock.SetupGet(ss => ss.Dispatcher).Returns(dispatcherScheduler);
+            var priceStream = new Subject<decimal>();
+            _myServiceMock.Setup(svc => svc.PriceStream(It.IsAny<string>())).Returns(priceStream);
+
+            var sut = new MyPresenter(_myServiceMock.Object, _viewModelMock.Object, _schedulerServiceMock.Object);
+            sut.Show("SomeSymbol");
+            threadPoolScheduler.Schedule(() => priceStream.OnNext(expected));
+            threadPoolScheduler.Run();
+
+            Assert.AreEqual(0, _viewModelMock.Object.Prices.Count);
+            dispatcherScheduler.Run();
+
+            Assert.AreEqual(1, _viewModelMock.Object.Prices.Count);
+            Assert.AreEqual(expected, _viewModelMock.Object.Prices.First());
+        }
+    }
+}
---
+++ trunk/src/RxSamples.Testing/08_TestingSheduling.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Concurrency;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RxSamples.Testing
+{
+    [TestClass]
+    public class TestingSheduling
+    {
+        [TestMethod]
+        public void Testing_with_real_scheduler()
+        {
+            Console.WriteLine("This test will take 5 seconds to run.");
+            var timer = Stopwatch.StartNew();
+            var interval = Observable
+              .Interval(TimeSpan.FromSeconds(1))
+              .Take(5);
+
+            bool isComplete = false;
+            interval
+                .Timeout(TimeSpan.FromSeconds(3))
+                .Subscribe(Console.WriteLine, () => isComplete = true);
+
+            while (!isComplete)
+            {
+
+            }
+            Console.WriteLine("Test completed in {0}.", timer.Elapsed);
+        }
+
+        [TestMethod]
+        public void Testing_with_test_scheduler()
+        {
+            Console.WriteLine("This test should take less than a second to run.");
+            var timer = Stopwatch.StartNew();
+            var scheduler = new TestScheduler();
+            var interval = Observable
+              .Interval(TimeSpan.FromSeconds(1), scheduler)
+              .Take(5);
+
+            bool isComplete = false;
+            interval
+                .Timeout(TimeSpan.FromSeconds(3))
+                .Subscribe(Console.WriteLine, () => isComplete = true);
+
+            scheduler.Run();
+
+            while (!isComplete)
+            {
+
+            }
+            Console.WriteLine("Test completed in {0}.", timer.Elapsed);
+        }
+
+        [TestMethod]
+        public void Testing_timeouts_with_the_TestScheduler()
+        {
+            var scheduler = new TestScheduler();
+            var stream = Observable.Never<int>();
+
+            var exceptionThrown = false;
+            //stream.Timeout(TimeSpan.FromMinutes(1))//If no scheduler is passed then Scheduler.ThreadPool is used as the default
+            stream.Timeout(TimeSpan.FromMinutes(1), scheduler)
+                .Subscribe(
+                    i => Console.WriteLine("This will ever run."),
+                    ex => exceptionThrown = true);
+            scheduler.Run();
+            Assert.IsTrue(exceptionThrown);
+        }
+    }
+}
