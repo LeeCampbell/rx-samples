bf04f4829ec427039f904560d0d1ddeba0499acc
Lee.Ryan.Campbell <Lee.Ryan.Campbell@be1c8d10-64b7-54c5-464f-300c71bfaafb>
2011-06-18 10:49:27 +0000

    Adding a spike that show cases how multiple versions of Rx can be referenced at the same time by various dependencies.

---
+++ trunk/MultiVersionSpike/src/ExampleLibrary.NewerVersion/SomeProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+
+namespace ExampleLibrary.NewerVersion
+{
+  public class SomeProvider
+  {
+    static SomeProvider()
+    {
+      DependencyResolver.Ensure();
+    }
+
+    public static IObservable<IList<long>> GetNumbers()
+    {
+      return Observable.Interval(TimeSpan.FromSeconds(1))
+        .Buffer(3); //Brand new to Rx_v1.0.10425
+    }
+  }
+}
---
+++ trunk/MultiVersionSpike/src/ExampleLibrary.OldVersion/DependencyResolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace ExampleLibrary.OldVersion
+{
+  internal static class DependencyResolver
+  {
+    private static int _isSet = 0;
+
+    internal static void Ensure()
+    {
+      if (Interlocked.CompareExchange(ref _isSet, 1, 0) == 0)
+      {
+        var thisAssembly = Assembly.GetExecutingAssembly();
+        var assemblyName = new AssemblyName(thisAssembly.FullName).Name;
+        var embededAssemblyPrefix = assemblyName + ".EmbeddedAssemblies.";
+
+        var myEmbeddedAssemblies =
+          Assembly.GetExecutingAssembly().GetManifestResourceNames()
+            .Where(name => name.StartsWith(embededAssemblyPrefix))
+            .Select(resourceName =>
+                      {
+                        using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+                        {
+                          var assemblyData = new Byte[stream.Length];
+                          stream.Read(assemblyData, 0, assemblyData.Length);
+                          return Assembly.Load(assemblyData);
+                        }
+                      })
+            .ToDictionary(ass => ass.FullName);
+
+        AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+                                                     {
+                                                       Assembly assemblyToLoad = null;
+                                                       myEmbeddedAssemblies.TryGetValue(args.Name, out assemblyToLoad);
+                                                       return assemblyToLoad;
+                                                     };
+      }
+    }
+  }
+}
